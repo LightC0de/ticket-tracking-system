13cccd565c509a8b378cfb17ddea06a177b03a2b
lightc0de <n0975771178@gmail.com>
2021-02-06 15:43:28 +0200

    Created pages and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
+import {TicketPageComponent} from './ticket-page/ticket-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'dashboard', component: DashboardPageComponent},
+      {path: 'create', component: CreatePageComponent},
+      {path: 'ticket/:id', component: TicketPageComponent},
+      {path: 'ticket/:id/edit', component: EditPageComponent},
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { TicketComponent } from './shared/components/ticket/ticket.component';
+import { TicketPageComponent } from './ticket-page/ticket-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent,
+    MainLayoutComponent,
+    LoginPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    TicketComponent,
+    TicketPageComponent
   ],
   imports: [
     BrowserModule,
